cba85c099e8153fac8d9bd01fd41a80a83a38e77
Max <48467432+Max-Frisch@users.noreply.github.com>
2025-06-07 15:11:32 +0700

    added fields and getters/setters to Customer class

---
+++ src/banking/BankAccount.java
@@ -0,0 +1,26 @@
+package banking;
+
+/**
+ * Represents a checking/savings bank account for a customer.
+ * @author Max
+ */
+public class BankAccount {
+	
+	//instance variables
+	
+	/**
+	 * Type of account (checking or savings)
+	 */
+	String accountType;
+	
+	/**
+	 * Balance for bank account.
+	 */
+	double balance;
+	
+	/**
+	 * Customer for this account.
+	 */
+	Customer customer;
+
+}
