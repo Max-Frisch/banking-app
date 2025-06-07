ba3c87afbb5e1dcc2c99f0255ff76ff2e182535f
Max <48467432+Max-Frisch@users.noreply.github.com>
2025-06-07 14:40:17 +0700

    Initial commit of the repo

---
+++ src/banking/Bank.java
@@ -0,0 +1,25 @@
+package banking;
+
+/**
+ * Represents a bank for managing customers and their bank accounts.
+ * @author Max
+ */
+public class Bank {
+
+	public static void main(String[] args) {
+		
+		//creates a new instance of the Bank class
+		Bank bank = new Bank();
+		
+		//calls the run method in the bank class
+		bank.run();
+	}
+	
+	/**
+	 * Runs the program by initializing and managing bank accounts and customers
+	 */
+	public void run() {
+		System.out.println("Welcome to the Banking app!");
+	}
+
+}
