671c1e20e9e900fc2e0792419b2bde4f45a974dc
Max <48467432+Max-Frisch@users.noreply.github.com>
2025-06-07 17:55:14 +0700

    refactoring of the Bank class, adding methods.

--- src/banking/Bank.java
+++ src/banking/Bank.java
@@ -1,10 +1,15 @@
 package banking;
 
+import java.util.Scanner;
+
 /**
  * Represents a bank for managing customers and their bank accounts.
  * @author Max
  */
 public class Bank {
+
+	//create new Scanner object
+	Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		
@@ -19,7 +24,99 @@ public class Bank {
 	 * Runs the program by initializing and managing bank accounts and customers
 	 */
 	public void run() {
+
 		System.out.println("Welcome to the Banking app!");
+		System.out.println("Creating a new Customer:");
+		System.out.println();
+		
+		System.out.print("What is your Name? ");
+		String customerName = scan.nextLine();
+		
+		System.out.println(); //blank line
+		System.out.println("Welcome, " + customerName + ". Creating checkings and savings accounts..");
+		
+		//create a new Customer with given name
+		Customer customer = new Customer(customerName);
+		
+		//get user address
+		System.out.print("Please input your Address: ");
+		String customerAddress = scan.nextLine();
+		customer.setAddress(customerAddress);
+		
+		//create bank accounts for customer
+		BankAccount savingsAccount = new BankAccount("savings", customer);
+		BankAccount checkingAccount = new BankAccount("checking", customer);
+		
+		System.out.println(); //blank line	
+		System.out.println("Savings and Checking accounts created for: " + savingsAccount.getCustomerInfo());
+		
+		//print current balances
+		//get and print account info for checking and savings account
+		printAccountBalances(checkingAccount, savingsAccount);
+		
+		//deposits
+		System.out.println(); //blank line
+		//into checking account
+		depositMoney(checkingAccount);
+		//into savings account
+		depositMoney(savingsAccount);
+		
+		//print current balances
+		//get and print account info for checking and savings account
+		printAccountBalances(checkingAccount, savingsAccount);
+		
+		//withdrawals
+		System.out.println(); //blank line	
+		//from checking account
+		withdrawMoney(checkingAccount);
+		//from savings account
+		withdrawMoney(savingsAccount);
+		
+		//print current balances
+		//get and print account info for checking and savings account
+		printAccountBalances(checkingAccount, savingsAccount);
+		
+		scan.close();
+	}
+	
+	/**
+	 * Attempt to withdraw prompted amount from given bank account
+	 * @param account
+	 */
+	private void withdrawMoney(BankAccount account) {
+		System.out.print("Amount (decimal) to be withdrawn from " + account.getAccountType() + " account: ");
+		double amount = scan.nextDouble();
+		try {
+			account.withdraw(amount);
+			System.out.println("Withdrawing " + amount + " successful.\n");
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	/**
+	 * Deposit 
+	 * @param account
+	 * @hidden
+	 */
+	private void depositMoney(BankAccount account) {
+		System.out.print("Amount (decimal) to deposit into your " + account.getAccountType() + " account: ");
+		double amount = scan.nextDouble();
+		account.deposit(amount);
+		System.out.println(amount + " has been deposited into " + account.getAccountType() + " account.");	
+	}
+
+	/**
+	 * Prints the balances of given savings and checking account
+	 * @param savingsAccount
+	 * @param checkingAccount
+	 */
+	public void printAccountBalances(BankAccount savingsAccount, BankAccount checkingAccount) {
+		//get and print account info for checking and savings account
+		System.out.println(); //blank line
+		System.out.println("Account balance: ");
+		System.out.println(savingsAccount.getAccountInfo());
+		System.out.println(checkingAccount.getAccountInfo());
 	}
 
 }
--- src/banking/BankAccount.java
+++ src/banking/BankAccount.java
@@ -22,5 +22,84 @@ public class BankAccount {
 	 * Customer for this account.
 	 */
 	Customer customer;
-
+	
+	//constructor
+	
+	/** Creates a bank account of given type for given customer
+	 * @param accountType - type of bank account
+	 * @param customer - customer/owner of bank account
+	 */
+	public BankAccount(String accountType, Customer customer) {
+		this.accountType = accountType;
+		this.customer = customer;
+	}
+	
+	//methods
+	
+	/**
+	 * Deposits given amount to the account balance.
+	 * @param amount - amount to be added to account balance
+	 */
+	public void deposit(double amount) {
+		this.balance += amount;
+	}
+	
+	/**
+	 * Withdraws given amount from the account balance.
+	 * @param amount - amount to be subtracted from account balance
+	 * @throws Exception if given amount is larger than available balance
+	 */
+	public void withdraw(double amount) throws Exception {
+		if (amount > this.balance) {
+			throw new Exception("Error! Amount " + amount + " is greater than available balance.\n");
+		}
+		this.balance -= amount;
+	}
+	
+	/**
+	 * Returns account type and balance for this bank account.
+	 * @return String with account info as <accountType>:<balance>
+	 */
+	public String getAccountInfo() {
+		return this.accountType + ": " + this.balance;
+	}
+	
+	/**
+	 * Returns the account type as "Checking" or "Savings"
+	 * @return String - account type
+	 */
+	public String getAccountType() {
+		return this.accountType;
+	}
+	
+	/**
+	 * Returns the customer name and address for this bank account.
+	 * @return String with customer name and address
+	 */
+	public String getCustomerInfo() {
+		return this.customer.getName() + " from " + this.customer.getAddress();
+	}
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
